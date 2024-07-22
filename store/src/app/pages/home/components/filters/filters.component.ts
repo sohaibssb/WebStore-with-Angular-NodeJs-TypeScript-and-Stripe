7287e56eda6f14fd0ae5abe095a401d1bb38c4d4
sohaibssb <sohayb.salem2012@gmail.com>
2024-07-22 17:45:50 +0300

    FixSomeBugs&AddNewComponents

--- store/src/app/pages/home/components/filters/filters.component.ts
+++ store/src/app/pages/home/components/filters/filters.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filters',
   templateUrl: 'filters.component.html' 
 })
 export class FiltersComponent implements OnInit {
-
-  categories = ['shoes', 'sports'];
+  @Output() showCategory = new EventEmitter<string>();
+  categories = ['Shoes', 'Sports'];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onShowCategory(category: string): void {
+    this.showCategory.emit(category);
   }
 
 }
---
+++ store/src/app/pages/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+  cols = 3;
+  category: string | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onColmunsCountChange(colsNum: number): void {
+    this.cols = colsNum;
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
+  }
+}
