405ec5c0c66dff613bda51e8ba16e21b549ca222
sohaibssb <sohayb.salem2012@gmail.com>
2024-07-30 05:16:09 +0300

    UpdateCartLogic

--- store/src/app/components/header/header.component.ts
+++ store/src/app/components/header/header.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-header',
-  templateUrl: `./header.component.html`,
-  styles: [
-  ]
+  templateUrl: `./header.component.html`
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  private _cart: Cart = { items: [] };
+  itemsQuantity = 0;
 
-  constructor() { }
+  @Input()
+  get cart(): Cart {
+    return this._cart;
+  }  
 
-  ngOnInit(): void {
+  set cart(cart: Cart) {
+    this._cart = cart;
+
+    this.itemsQuantity = cart.items
+      .map((item) => item.quantity)
+      .reduce((prev, current) => prev + current, 0);
   }
 
+  constructor(private cartService: CartService) { }
+
+  getTotal(items: Array<CartItem>): number {
+    return this.cartService.getTotal(items);
+  }
+
+  onClearCart() {
+    this.cartService.clearCart();
+  }
 }
